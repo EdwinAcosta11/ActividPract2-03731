bd8d07e529ccd41f1cf667f2ad852ab3c87b3fbd
EdwinAcosta11 <elimperiar@gmail.com>
2025-06-08 17:17:54 -0400

    Agregando ejercicio 8

---
+++ Practica2Ejerc84318/ApiComentarios/Controllers/CommentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiComentarios.Models;
+
+namespace ApiComentarios.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentsController : ControllerBase
+    {
+        private static List<Comentario> comentarios = new();
+        private static int contadorId = 1;
+
+        // GET: api/comments
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(comentarios);
+        }
+
+        // POST: api/comments
+        [HttpPost]
+        public IActionResult Post([FromBody] Comentario comentario)
+        {
+            comentario.Id = contadorId++;
+            comentario.Fecha = DateTime.Now;
+            comentarios.Add(comentario);
+            return CreatedAtAction(nameof(Get), new { id = comentario.Id }, comentario);
+        }
+
+        // DELETE: api/comments/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var comentario = comentarios.FirstOrDefault(c => c.Id == id);
+            if (comentario == null)
+                return NotFound();
+
+            comentarios.Remove(comentario);
+            return NoContent();
+        }
+    }
+}
+
